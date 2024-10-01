ac7c4e3f763e75980ff740fb7fd05a82bf1db700
Andrei Nagy <56216769+andrei-nagy@users.noreply.github.com>
2024-10-01 09:59:38 +0300

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
 
-    const frontend_url = "https://food-delivery-frontend-vruc.onrender.com/";
+  //  const frontend_url = "https://food-delivery-frontend-vruc.onrender.com/";
 
     try {
         const newOrder = new orderModel({
