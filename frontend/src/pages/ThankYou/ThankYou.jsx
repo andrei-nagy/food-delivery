8c0105543f5d4f9c528018f8cd0757372f9b6adc
Andrei Nagy <56216769+andrei-nagy@users.noreply.github.com>
2024-10-13 12:02:16 +0300

    Update ThankYou.jsx v3

--- frontend/src/pages/ThankYou/ThankYou.jsx
+++ frontend/src/pages/ThankYou/ThankYou.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './ThankYou.css'; // Stiluri
 import { toast } from 'react-toastify';
 import { StoreContext } from '../../context/StoreContext';
