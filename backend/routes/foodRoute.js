8cccccc106a3dcad0d0f5456600cecb0eef18c53
andrei-nagy <andreixfr@gmail.com>
2025-07-21 22:27:06 +0300

    fix trying

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,17 +1,22 @@
 import express from "express";
 import { addFood, listFood, removeFood, updateFood } from "../controllers/foodController.js";
 import multer from "multer";
+import path from 'path';
 
 const foodRouter = express.Router();
 
 //Image storage engine
 
 const storage = multer.diskStorage({
-    destination: "uploads",
-    filename: (req, file, cb) => {
-        return cb(null, `${Date.now()}${file.originalname}`)
-    }
-})
+  destination: (req, file, cb) => {
+    const uploadPath = path.resolve('uploads');
+    console.log("Upload destination path:", uploadPath);
+    cb(null, uploadPath);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  }
+});
 
 const upload = multer({ storage: storage })
 
